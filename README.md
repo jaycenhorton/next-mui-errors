89c9347ce8637695ee0baa71d88c3833e64397bf
jaycen horton <jaycen.horton@gmail.com>
2019-04-26 16:16:04 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# steps to repro
+- yarn build
+- yarn start
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { List, ListItem } from '@material-ui/core';
+
+const Home = () => (
+  <List>
+    <ListItem>test</ListItem>
+  </List>
+);
+
+export default Home;
